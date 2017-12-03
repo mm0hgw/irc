ee3d4881227d7bb30e71e61da12585d284351d05
mm0hgw@gmail.com <dale@piratepress.org>
2017-12-03 03:56:34 +0000

    buildPackage get.lapply 0.0.0.9000

--- get.lapply/R/get.lapply.R
+++ get.lapply/R/get.lapply.R
@@ -10,8 +10,8 @@ get.lapply.env <- new.env()
 #'@description
 #'Get a lapply
 #'@export
-get.lapply <- function(){
-	get("lapply",envir=get.lapply.env)
+get.lapply <- function() {
+    get("lapply", envir = get.lapply.env)
 }
 
 #'set.lapply
@@ -19,17 +19,17 @@ get.lapply <- function(){
 #'Set the served lapply.
 #'@param x a 'function'
 #'@export
-set.lapply <- function(x){
-	stopifnot(is.function(x))
-	assign("lapply",x,envir=get.lapply.env)
+set.lapply <- function(x) {
+    stopifnot(is.function(x))
+    assign("lapply", x, envir = get.lapply.env)
 }
 
 #'get.seeded.lapply
 #'@description
 #'Get a lapply
 #'@export
-get.seeded.lapply <- function(){
-	get("seeded.lapply",envir=get.lapply.env)
+get.seeded.lapply <- function() {
+    get("seeded.lapply", envir = get.lapply.env)
 }
 
 #'set.seeded.lapply
@@ -37,17 +37,17 @@ get.seeded.lapply <- function(){
 #'Set the served lapply.
 #'@param x a 'function'
 #'@export
-set.seeded.lapply <- function(x){
-	stopifnot(is.function(x))
-	assign("seeded.lapply",x,envir=get.lapply.env)
+set.seeded.lapply <- function(x) {
+    stopifnot(is.function(x))
+    assign("seeded.lapply", x, envir = get.lapply.env)
 }
 
 #'get.chunkSize
 #'@description
 #'Get a chunkSize
 #'@export
-get.chunkSize <- function(){
-	get("chunkSize",envir=get.lapply.env)
+get.chunkSize <- function() {
+    get("chunkSize", envir = get.lapply.env)
 }
 
 #'set.chunkSize
@@ -55,11 +55,11 @@ get.chunkSize <- function(){
 #'Set the served chunkSize
 #'@param x an 'integer'
 #'@export
-set.chunkSize <- function(x){
-	stopifnot(length(x)==1)
-	x <- as.integer(x)
-	stopifnot(is.integer(x))
-	assign("chunkSize",x,envir=get.lapply.env)
+set.chunkSize <- function(x) {
+    stopifnot(length(x) == 1)
+    x <- as.integer(x)
+    stopifnot(is.integer(x))
+    assign("chunkSize", x, envir = get.lapply.env)
 }
 
 set.chunkSize(.Machine$integer.max)
@@ -68,14 +68,14 @@ set.seeded.lapply(lapply)
 
 #'get.sensible.threads
 #'@export
-get.sensible.threads <- function(){
-	get('sensible.threads',envir=get.lapply.env)
+get.sensible.threads <- function() {
+    get("sensible.threads", envir = get.lapply.env)
 }
 #'set.sensible.threads
 #' @importfrom parallel detectCores
 #'@export
-set.sensible.threads <- function(x=min(1,parallel::detectCores()-1)){
-	assign('sensible.threads',x)
+set.sensible.threads <- function(x = min(1, parallel::detectCores() - 1)) {
+    assign("sensible.threads", x)
 }
 
-if(!exists('sensible.threads',envir=get.lapply.env)) set.sensible.threads()
+if (!exists("sensible.threads", envir = get.lapply.env)) set.sensible.threads()
